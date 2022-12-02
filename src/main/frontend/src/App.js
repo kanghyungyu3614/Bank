b92fc1e7a9b80c928be907990368af70e01252b4
강현규 <kanghungu3614@gmail.com>
2022-12-02 16:25:38 +0900

    초기세팅

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div> 리액트 초기화면 입니다.</div>
+    </div>
+  );
+}
+
+export default App;
