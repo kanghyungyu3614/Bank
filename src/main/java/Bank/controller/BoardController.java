0b9a6029b123c947b94b6e76dd18837f0c4c7924
최윤미 <dive@gmail.com>
2022-12-05 18:52:31 +0900

    12/05 등록기능 썸머노트 설정

--- src/main/java/Bank/controller/BoardController.java
+++ src/main/java/Bank/controller/BoardController.java
@@ -1,8 +1,11 @@
 package Bank.controller;
 
+import Bank.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bank")
 public class BoardController {
 
- /*   @GetMapping("/board")
-    public Resource gethome(){
+    /*----------------------- 페이징 처리---------------*/
+
+    @GetMapping("/board")
+    public Resource getboard(){
         return new ClassPathResource("templates/board.html");
-      }*/
+      }
+
+    @GetMapping("/write")
+    public Resource getwrite(){
+        return new ClassPathResource("templates/write.html");
+    }
+
+    @Autowired
+    private BoardService boardService = new BoardService();
+
+
+    //*--------------요청과 응답처리 ------------------------*//
+
+   // @PostMapping("/setboard")
+   /// public boolean setboard( BoardDto boardDto ){
+    //    return boardService.setboard( boardDto);
+  //  }
+
+
+
+
 
 }
--- src/main/java/Bank/service/BoardService.java
+++ src/main/java/Bank/service/BoardService.java
@@ -1,7 +1,25 @@
 package Bank.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Service
 public class BoardService {
+
+    //*--------------1. 전역변수 ------------------------*//
+    @Autowired
+    private HttpServletRequest request; // 요청객체선언
+
+    @Autowired
+    private HttpServletResponse response; // 응답 객체 선언
+
+
+    //*--------------2. 서비스 ------------------------*//
+
+
+
+
 }
