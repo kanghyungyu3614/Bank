cb3771eaaff9d3c167ac82075248e8135801a52c
강현규 <kanghungu3614@gmail.com>
2022-12-06 17:18:33 +0900

    강현규test

--- src/main/frontend/src/component/SecurityCard.jsx
+++ src/main/frontend/src/component/SecurityCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './css/SecurityCard.css'
 import axios from 'axios'; //   npm install axios 설치 했을경우만 가능
 
@@ -7,26 +7,38 @@ export default function SecurityCard (){
     //  @CrossOrigin(origins = "http://localhost:3000")
     let data =  axios.get( "http://localhost:8080/member/securityCard") // 요청
                 .then( res => { alert( res.data);console.log(res);} )                    // 응답
-    for(let i=0; i<data.length; i++){
 
 
+    function PrintSecurityCardInfo(){
+        for(let i=0; i<data.length; i++){
+
+
+        }
     }
+
+
 
   return (
     <div className="securitCard">
         {/* main number */}
         <div className="main">
             <div>피싱사기 주의</div>
-            <div className="mainSecutiryNumber">0000 00 0000</div>
+            <div className="mainSecutiryNumber">0000 00 0000{/*data.mainNumber*/}</div>
         </div>
 
         {/* sub number */}
         <div className="sub">
-            <div>test해보겠습니다.</div>
-            <div>test해보겠습니다.</div>
-            <div>test해보겠습니다.</div>
-            <div>test해보겠습니다.</div>
-            <div>test해보겠습니다.</div>
+            <div>1.|00 00 00</div>
+            <div>2.|00 00 00</div>
+            <div>3.|00 00 00</div>
+            <div>4.|00 00 00</div>
+            <div>5.|00 00 00</div>
+            <div>6.|00 00 00</div>
+            <div>7.|00 00 00</div>
+            <div>8.|00 00 00</div>
+            <div>9.|00 00 00</div>
+            <div>10.|00 00 00</div>
+            {/*PrintSecurityCardInfo()*/}
         </div>
     </div>
     );
