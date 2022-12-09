33070c5f76b18f21227dc17a2e653e48d3c66367
최윤미 <dive@gmail.com>
2022-12-09 18:10:52 +0900

    12/09 게시판 수정,삭제 기능

--- src/main/java/Bank/controller/BoardController.java
+++ src/main/java/Bank/controller/BoardController.java
@@ -5,10 +5,7 @@ import Bank.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,18 +20,21 @@ public class BoardController {
 
     /*----------------------- 페이징 처리---------------*/
 
-    @GetMapping("/board")
+    @GetMapping("/board")  // 기본 전체 게시판
     public Resource getboard(){
         return new ClassPathResource("templates/board.html");
       }
 
-    @GetMapping("/write")
+    @GetMapping("/write")  // 글 등록
     public Resource getwrite(){
         return new ClassPathResource("templates/write.html");
     }
 
-   @GetMapping("/bdlist")
+   @GetMapping("/bdlist") // 자세히보기
    public Resource getdlist() { return new ClassPathResource("templates/bdetail.html"); }
+
+    @GetMapping("/bupdate")
+    public Resource getupdate() { return new ClassPathResource("templates/bupdate.html");}
 
     //*--------------요청과 응답처리 ------------------------*//
 
@@ -52,10 +52,22 @@ public class BoardController {
          return boardService.blist( page );
     }
 
-    @GetMapping("/detail")
+    @GetMapping("/bdetail") // 게시물 개별 조회
     public BboardDto bdtail( int bno){
 
         return boardService.bdtail( bno);
+    }
+
+
+    @DeleteMapping("/bdelete")
+    public boolean bdlete  (int bno){
+        return boardService.bdelete( bno);
+    }
+
+
+    @PutMapping("/bupdate")
+     public boolean bupdate( BboardDto boardDto){
+        return boardService.bupdate( boardDto );
     }
 
 
