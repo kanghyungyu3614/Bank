e4a519a62b2bb3d317ce5a57612266f383742997
강현규 <kanghungu3614@gmail.com>
2022-12-12 13:14:48 +0900

    강현규 2022-12-12 거래내역 이동까지 가능 이제 리스트들 불러오면 됩니다.

--- src/main/frontend/src/component/security/SecurityComponent/SecurityMainNumber.jsx
+++ src/main/frontend/src/component/security/SecurityComponent/SecurityMainNumber.jsx
@@ -4,9 +4,9 @@ export default function SecurityMainNumber(props){
 console.log("SecurityMainNumber 안의 props 잘들어오니?" + props.props);
 let Coma = String(props.props);
 
-let finalComa = "No. "+Coma;
+let finalComa =Coma;
 {/*No. 8783 76 9934 {"No. "+Coma}*/}
 return (
-    <div className="mainSecutiryNumber">{finalComa}</div>
+    <div className="mainSecutiryNumber">{"No. "+finalComa.substring(0,4)+" "+finalComa.substring(4,6)+" "+finalComa.substring(6)}</div>
 );
 }
