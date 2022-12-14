7fdd6b7a3987d59dc276a2c539639ba6d848914a
강현규 <kanghungu3614@gmail.com>
2022-12-14 16:51:51 +0900

    강현규 2022-12-14 보안카드 완성

--- src/main/frontend/src/component/Bank/DealReport.jsx
+++ src/main/frontend/src/component/Bank/DealReport.jsx
@@ -1,65 +1,65 @@
-import React, { useState, useEffect, useRef } from 'react';
-import '../css/DealReport.css'
-import { useNavigate } from 'react-router-dom'; // 설치한 패키지// 참고 : 6버전에서 useNavigate로 변경되었다.
-import axios from 'axios';
-import DealListComponent from './DealReportComponent/DealListComponent';
-import Pagination from 'react-js-pagination' // npm i react-js-pagination 설치
+import React, { useState, useEffect, useRef } from'react';
+import'../css/DealReport.css'
+import { useNavigate } from'react-router-dom';//설치한 패키지//참고 : 6버전에서 useNavigate로 변경되었다.
+import axios from'axios';
+import DealListComponent from'./DealReportComponent/DealListComponent';
+import Pagination from'react-js-pagination'// npm i react-js-pagination설치
 
 /*강현규 2022-12-12 admin에서의 user들의 거래내역 출력*/
 export default function DealReport (){
 
-    const [ pageInfo , setPageInfo ] = useState({  page : 1 , key : "" , keyword:""  }) // 1. 요청 정보 객체 state
-    const [ pageDto , setPageDto ] = useState( { list : [] } )// 1. 게시물 리스트 state
+    const [ pageInfo , setPageInfo ] = useState({  page : 1 , key :"", keyword:""})// 1.요청 정보 객체 state
+const [ pageDto , setPageDto ] = useState( { list : [] } )// 1.게시물 리스트 state
     // [ ] : array/list     {  } : object/dto
-    // ------------------------------  1. 게시물  -------------------------------------- //
-    function getboardlist( ){ // 2. server : pageInfo 요청 => pageDto 응답 [ 실행조건 : 1. 렌더링될때 2.검색할때 3.카테고리선택 4.페이징 선택  ---> 일반 함수화 ]
-        axios.post( "/bank/dealReport/boardlist" ,  pageInfo )
+    // ------------------------------  1.게시물  -------------------------------------- //
+function getboardlist( ){// 2. server : pageInfo요청 => pageDto응답 [실행조건 : 1.렌더링될때 2.검색할때 3.카테고리선택 4.페이징 선택  --->일반 함수화 ]
+axios.post("/bank/dealReport/boardlist",  pageInfo )
              .then( res => {
-             console.log( "/bank/dealReport/boardlist 경로" );
-             console.log( "res.data" );
+             console.log("/bank/dealReport/boardlist경로");
+             console.log("res.data");
              console.log( res.data );
              setPageDto( res.data );
              })
              .catch( err => { console.log( err ); } )
     }
-    useEffect( getboardlist , [ pageInfo ] )  // 3. 렌더링 될때 그리고 *** pageInfo 변경될때 마다
+    useEffect( getboardlist , [ pageInfo ] )// 3.렌더링 될때 그리고 *** pageInfo변경될때 마다
 
 
-    // ------------------------------  3.페이징  -------------------------------------- //
-    const onPage = ( page ) =>{
+// ------------------------------  3.페이징  -------------------------------------- //
+const onPage = ( page ) =>{
         setPageInfo(
             {   page : page ,
-                key : pageInfo.key ,            // 기존 검색 필드명
-                keyword : pageInfo.keyword  }   // 기존 검색할 단어
-        )
+                key : pageInfo.key ,//기존 검색 필드명
+keyword : pageInfo.keyword  }//기존 검색할 단어
+)
     }
-    // --------------------------------------------------------------------------------- //
-    // ------------------------------  4. 검색   -------------------------------------- //
-    const onSearch = () => {
+// --------------------------------------------------------------------------------- //
+    // ------------------------------  4.검색   -------------------------------------- //
+const onSearch = () => {
         setPageInfo(
-            {   page : 1 ,              // 검색시 첫페이지부터 보여주기 [ 1 ]
-                key : document.querySelector('.key').value ,    // 검색할 필드명
-                keyword: document.querySelector('.keyword').value  } // 검색할 단어
-        )
+            {   page : 1 ,//검색시 첫페이지부터 보여주기 [ 1 ]
+key : document.querySelector('.key').value ,//검색할 필드명
+keyword: document.querySelector('.keyword').value  }//검색할 단어
+)
     }
-    // --------------------------------끝------------------------------------- //
-     const [ dealReportMainData , setDealReportMainData] = useState([])
+// --------------------------------끝------------------------------------- //
+const [ dealReportMainData , setDealReportMainData] = useState([])
      useEffect(()=>{
-     axios.get( "http://localhost:8080/bank/dealReport") // 요청
-                  .then( res => {
-                          console.log("res.data는?" + res.data);
+     axios.get("http://localhost:8080/bank/dealReport")//요청
+.then( res => {
+                          console.log("res.data는?"+ res.data);
                           console.log(res.data);
                           setDealReportMainData([...res.data])
                           console.log("axios안의 dealReportMainData");
                           console.log(dealReportMainData);
-         })// 응답
-         axios.post("http://localhost:8080/bank/dealReport")
+         })//응답
+axios.post("http://localhost:8080/bank/dealReport")
 
 
      },[])
          console.log("axios밖의 dealReportMainData");
          console.log(dealReportMainData);
-         return(
+return(
              <div className="dealReport">
              <div className="dealReportComtent">
                  <div className="ContentText searchBox">
@@ -76,8 +76,8 @@ export default function DealReport (){
                          <option value="bacba">통장에남은금액</option>
                          <option value="bacst">휴먼상태</option>
                      </select>
-                     <input type="text" className="userNameinput keyword"/>
-                     <button type="button" className="userButton" onClick={ onSearch }>검색</button>
+                     <input type="text"className="userNameinput keyword"/>
+                     <button type="button"className="userButton"onClick={ onSearch }>검색</button>
                  </div>
              </div>
              <DealListComponent props={dealReportMainData}/>
--- src/main/frontend/src/component/security/SecurityComponent/SecurityMainNumber.jsx
+++ src/main/frontend/src/component/security/SecurityComponent/SecurityMainNumber.jsx
@@ -1,19 +1,19 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect} from'react';
 
 export default function SecurityMainNumber( props ){
     let result;
     const [ ResultComa , setResultComa] = useState('');
 
     useEffect(()=>{
-        if(props.props.length>0){
-            let finalComa =props.props;
-            result = "No. "+finalComa.substring(0,4)+" "+finalComa.substring(4,6)+" "+finalComa.substring(6);
-            setResultComa(result)
-        }
-    },[props.props])
+    if(props.props.length>0){
+                let finalComa =props.props;
+                result ="No. "+finalComa.substring(0,4)+" "+finalComa.substring(4,6)+" "+finalComa.substring(6);
+                setResultComa(result)
+            }
+        },[props.props])
 
-{/*No. 8783 76 9934 {"No. "+Coma}*/}
-return (
-    <div className="mainSecutiryNumber">{ResultComa}</div>
-);
+    {/*No. 8783 76 9934 {"No. "+Coma}*/}
+    return(
+        <div className="mainSecutiryNumber">{ResultComa}</div>
+    );
 }
