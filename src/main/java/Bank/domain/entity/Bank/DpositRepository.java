a044f95e0924143ce50ec74f2260799b5c741901
kimkyoungju <rlarudwn900@naver.com>
2022-12-19 18:00:11 +0900

    2022-12-19 김경주 jsx 작업 및 머지, 백엔드 및  엔티티 연결작업

--- src/main/java/Bank/domain/entity/Bank/DpositRepository.java
+++ src/main/java/Bank/domain/entity/Bank/DpositRepository.java
@@ -9,4 +9,7 @@ public interface DpositRepository extends JpaRepository<DpositEntity,Integer> {
     // 1.  보안카드번호검색
     @Query( value = "select * from dposit where ano = :ano" , nativeQuery = true )
     List<DpositEntity> findAcno(int ano);
+
+    @Query(value = "select * from dposit where acno = :acno" , nativeQuery = true  )
+    DpositEntity findacno(String ainput );
 }
