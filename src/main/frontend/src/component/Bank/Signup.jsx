25bfca788630d8904daf5940ad82c835e5fe5942
김원종 <mz1693@naver.com>
2022-12-20 21:38:39 +0900

    2022-12-20 김원종 카카오 주소 넘기기 구현중

--- src/main/frontend/src/component/Bank/Signup.jsx
+++ src/main/frontend/src/component/Bank/Signup.jsx
@@ -1,70 +1,31 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import "../css/Signup.css"
-import DaumPostcode from "react-daum-postcode";
+import DaumPostcode, {useDaumPostcodePopup} from "react-daum-postcode";
 
 export default function Signup(props) {
 
     /*===========================주소API========================================*/
-    const Post = (props) => {
+    const [address, setAddress] = useState({name: ''})
+    const open = useDaumPostcodePopup("//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js")
 
-        const complete = (data) =>{
-            let fullAddress = data.address;
-            let extraAddress = '';
-
-            if (data.addressType === 'R') {
-                if (data.bname !== '') {
-                    extraAddress += data.bname;
-                }
-                if (data.buildingName !== '') {
-                    extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
-                }
-                fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
-            }
-            console.log(data)
-            console.log(fullAddress)
-            console.log(data.zonecode)
-
-            props.setcompany({
-                ...props.company,
-                address:fullAddress,
-            })
-        }
-
-        return (
-            <div >
-                <DaumPostcode
-                    className="postmodal"
-                    autoClose
-                    onComplete={complete} />
-            </div>
-        );
+    const handleClick = () => {
+        open({onComplete: handleComplete});
     };
 
-
-    const [enroll_company, setEnroll_company] = useState({
-        address:'',
-    });
-
-    let [popup, setPopup] = useState(false);
-
-    const handleInput = (e) => {
-        setEnroll_company({
-            ...enroll_company,
-            [e.target.name]:e.target.value,
-
-        })
-    }
-
     const handleComplete = (data) => {
-        setPopup(!popup);
-        let user_enroll_text=document.querySelector('.user_enroll_text')
-        console.log(user_enroll_text.value)
-        if(user_enroll_text.value!=null){
-        setPopup(!popup);
-        }
-    }
 
+        axios.get("https://dapi.kakao.com/v2/local/search/address.json?query=" + data.address
+            , {headers: {Authorization: 'KakaoAK b9157166d1587f60a8ff9bf7e7c9a4f1'}})
+            .then(res => {
+                const location = res.data.documents[0]
+                console.log("확인하자")
+                console.log(location)
+                //5. State 업데이트
+                setAddress({name: data.address, lat: location.y, lng: location.x})
+                setForm({...form, name: data.address })
+            })
+    };
     /*===========================================================================*/
     const signUp = () => {
 
@@ -91,8 +52,8 @@ export default function Signup(props) {
         mphone: '',
         msno: '',
         mname: '',
+        madress: '',
     })
-
     const [confirm, setConfirm] = useState({ // form의 input의 객체값이 전부 true여야 통과
         midc: false,
         mpwc: false,
@@ -100,6 +61,7 @@ export default function Signup(props) {
         mphonec: false,
         msnoc: false,
         mnamec: false,
+        madressc: false,
     })
 
     const midform = /^[A-Za-z0-9$@!%*#?&]{6,15}$/; // 정규표현식 아이디[ 2022-12-16 김원종 ]
@@ -111,7 +73,6 @@ export default function Signup(props) {
 
 
     const midcheck = (e) => { /*아이디 검사 [ 2022-12-16 ] 김원종 */
-        console.log(e.target.value)
 
         setForm({...form, mid: e.target.value})
 
@@ -149,7 +110,6 @@ export default function Signup(props) {
     }
     const phonecheck = (e) => {/*전화번호 확인 [2022-12-16] 김원종 */
         setForm({...form, mphone: e.target.value})
-        console.log(e.target.value)
         let phckbox = document.querySelector('.phckbox')
         if (phoneform.test(e.target.value)) {
             phckbox.innerHTML = "📞올바른 형식입니다><"
@@ -161,7 +121,6 @@ export default function Signup(props) {
     }
     const msnocheck = (e) => {/*주민번호 확인 [2022-12-16] 김원종 */
         setForm({...form, msno: e.target.value})
-        console.log(e.target.value)
         let msnockbox = document.querySelector('.msnockbox')
         if (msnoform.test(e.target.value)) {
             msnockbox.innerHTML = "👌올바른 형식입니다.👌"
@@ -174,7 +133,6 @@ export default function Signup(props) {
 
     const mnamecheck = (e) => {/*이름 확인 [2022-12-16] 김원종 */
         setForm({...form, mname: e.target.value})
-        console.log(e.target.value)
         let mnameckbox = document.querySelector('.mnameckbox')
         if (mnameform.test(e.target.value)) {
             mnameckbox.innerHTML = "반가워요 멋진 이름이네요!🙌"
@@ -185,71 +143,88 @@ export default function Signup(props) {
         }
     }
 
-        return (
-            <div>
-                <h3 className="top_title "> Welcome_<br/>
-                    Team_Bank</h3>
-                <form className="bankform">
-                    <label className="text-bg-center">👉아이디[ 영대소문자,특수문자,숫자 포함 6~15자리로 작성해주세요😀]</label>
-                    <input type="text" name="mid" value={form.mid} className="form-control"
-                           onChange={(e) => midcheck(e)}/>
-                    <div>
-                        <span className="idbox"></span>
-                    </div>
-                    <label className="text-bg-center">👉비밀번호 [최소 8 자 및 최대 10 자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상으로
-                        작성해주세요😁]</label>
-                    <input type="text"
-                           className="form-control" value={form.mpw} name="mpw" onChange={(e) => pwcheck(e)}/>
-                    <div>
-                        <span className="pwbox"></span>
-                    </div>
-
-                    <label className="text-bg-center">👉비밀번호확인</label>
-                    <input type="text"
-                           className="form-control" value={form.mpwconfirm} name="mpwconfirm"
-                           onChange={(e) => pwfirmcheck(e)}/>
-                    <div>
-                        <span className="pwchbox"></span>
-                    </div>
-
-                    <label className="text-bg-center">👉전화번호</label>
-                    <input type="text" value={form.mphone}
-                           className="form-control " name="mphone" onChange={(e) => phonecheck(e)}/>
-                    <div>
-                        <span className="phckbox"></span>
-                    </div>
-
-                    <label className="text-bg-center">👉주민번호[ - 을 제외하고 적어주세요 ]</label>
-                    <input type="password" value={form.msno}
-                           className="form-control " name="msno" onChange={(e) => msnocheck(e)}/>
-                    <div>
-                        <span className="msnockbox"></span>
-                    </div>
-                    <label className="text-bg-center">👉이름</label>
-                    <input type="text" value={form.mname}
-                           className="form-control " name="mname" onChange={(e) => mnamecheck(e)}/>
-                    <div>
-                        <span className="mnameckbox"></span>
-                    </div>
-
-                    <label className="text-bg-center">👉주소</label> {/*2022-12-19 김원종 주소 api 구현중*/}
-                    <div className="address_search" >
-                        <input className="form-control user_enroll_text" placeholder="주소"  type="text" required={true} name="address" onChange={handleInput} value={enroll_company.address}/>
-                        <button onClick={handleComplete}>우편번호 찾기</button>
-                        {popup && <Post company={enroll_company} setcompany={setEnroll_company} />}
-                    </div>
 
 
-                 {/*   {openPostcode &&
-                        <DaumPostcode
-                            onComplete={handle.selectAddress}  // 값을 선택할 경우 실행되는 이벤트
-                            autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
-                            defaultQuery='판교역로 235' // 팝업을 열때 기본적으로 입력되는 검색어
-                        />}*/}
 
-                    <button type="button" onClick={signUp}>Go</button>
-                </form>
-            </div>
-        );
+    const madresscheck = (e) => {
+        console.log(".....////////////////////////////")
+        // let test = document.querySelector('.madress').value
+        console.log(e.target.value)
+        if (e.target.value != null) {
+            confirm.madressc = true;
+        } else {
+            alert("주소값을 입력해주세요")
+        }
     }
+
+    // useEffect(madresscheck ,[asd])
+
+
+    return (
+        <div>
+            <h3 className="top_title "> Welcome_<br/>
+                Team_Bank</h3>
+            <form className="bankform">
+                <label className="text-bg-center">👉아이디[ 영대소문자,특수문자,숫자 포함 6~15자리로 작성해주세요😀]</label>
+                <input type="text" name="mid" maxLength="15" value={form.mid} className="form-control"
+                       onChange={(e) => midcheck(e)}/>
+                <div>
+                    <span className="idbox"></span>
+                </div>
+                <label className="text-bg-center">👉비밀번호 [최소 8 자 및 최대 10 자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상으로
+                    작성해주세요😁]</label>
+                <input type="password"
+                       maxLength="15" className="form-control" value={form.mpw} name="mpw"
+                       onChange={(e) => pwcheck(e)}/>
+                <div>
+                    <span className="pwbox"></span>
+                </div>
+
+                <label className="text-bg-center">👉비밀번호확인</label>
+                <input type="password"
+                       className="form-control" value={form.mpwconfirm} name="mpwconfirm"
+                       onChange={(e) => pwfirmcheck(e)}/>
+                <div>
+                    <span className="pwchbox"></span>
+                </div>
+
+                <label className="text-bg-center">👉전화번호</label>
+                <input type="text" value={form.mphone}
+                       className="form-control " name="mphone" maxLength="13" onChange={(e) => phonecheck(e)}/>
+                <div>
+                    <span className="phckbox"></span>
+                </div>
+
+                <label className="text-bg-center">👉주민번호[ - 을 제외하고 적어주세요 ]</label>
+                <input type="password" value={form.msno}
+                       className="form-control " name="msno" maxLength="13" onChange={(e) => msnocheck(e)}/>
+                <div>
+                    <span className="msnockbox"></span>
+                </div>
+                <label className="text-bg-center">👉이름</label>
+                <input type="text" value={form.mname}
+                       className="form-control " name="mname" onChange={(e) => mnamecheck(e)}/>
+                <div>
+                    <span className="mnameckbox"></span>
+                </div>
+
+                <label className="text-bg-center">👉주소</label> {/*2022-12-19 김원종 주소 api 구현중*/}
+                <div className="address_search">
+                    <input  className="form-control madress"  value={form.madress=address.name} placeholder="주소" type="text"
+                           onChange={(e) => {
+                               madresscheck(e);
+                           }}/>
+                    <button type='button' onClick={handleClick}>
+                        위치찾기
+                    </button>
+                </div>
+
+
+
+
+                <button type="button" onClick={signUp}>Go</button>
+            </form>
+        </div>
+    );
+}
 
