37cc0fa9f3799f80bd688f91a87cb4280a7b6e55
김원종 <mz1693@naver.com>
2022-12-21 20:16:21 +0900

    2022-12-21 김원종 카카오 주소 오류 수정중

--- src/main/frontend/src/component/Bank/Signup.jsx
+++ src/main/frontend/src/component/Bank/Signup.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import "../css/Signup.css"
-import DaumPostcode, {useDaumPostcodePopup} from "react-daum-postcode";
+import DaumPostcode, {useDaumPostcodePopup  } from "react-daum-postcode";
+import DaumPostcodeEmbed from "react-daum-postcode";
+
 
 export default function Signup(props) {
 
@@ -23,9 +25,96 @@ export default function Signup(props) {
                 console.log(location)
                 //5. State 업데이트
                 setAddress({name: data.address, lat: location.y, lng: location.x})
-                setForm({...form, name: data.address })
+                setForm({...form, name: data.address})
             })
     };
+    /*===========================================================================*/
+    /*카카오 주소 api*/
+
+
+    const element_layer = useRef( null );
+
+    function closeDaumPostcode() {
+        // iframe을 넣은 element를 안보이게 한다.
+        element_layer.style.display = 'none';
+    }
+    function sample2_execDaumPostcode() {
+        new DaumPostcodeEmbed({
+            oncomplete: function(data) {
+                // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var addr = ''; // 주소 변수
+                var extraAddr = ''; // 참고항목 변수
+
+                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                    addr = data.roadAddress;
+                } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                    addr = data.jibunAddress;
+                }
+
+                // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+                if(data.userSelectedType === 'R'){
+                    // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                    // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                    if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                        extraAddr += data.bname;
+                    }
+                    // 건물명이 있고, 공동주택일 경우 추가한다.
+                    if(data.buildingName !== '' && data.apartment === 'Y'){
+                        extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                    }
+                    // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                    if(extraAddr !== ''){
+                        extraAddr = ' (' + extraAddr + ')';
+                    }
+                    // 조합된 참고항목을 해당 필드에 넣는다.
+                    document.getElementById("sample2_extraAddress").value = extraAddr;
+
+                } else {
+                    document.getElementById("sample2_extraAddress").value = '';
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById('sample2_postcode').value = data.zonecode;
+                document.getElementById("sample2_address").value = addr;
+                // 커서를 상세주소 필드로 이동한다.
+                document.getElementById("sample2_detailAddress").focus();
+
+                // iframe을 넣은 element를 안보이게 한다.
+                // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
+                element_layer.style.display = 'none';
+            },
+            width : '100%',
+            height : '100%',
+            maxSuggestItems : 5
+        }).embed(element_layer);
+
+
+        // iframe을 넣은 element를 보이게 한다.
+        element_layer.style.display = 'block';
+
+        // iframe을 넣은 element의 위치를 화면의 가운데로 이동시킨다.
+        initLayerPosition();
+    }
+    // 브라우저의 크기 변경에 따라 레이어를 가운데로 이동시키고자 하실때에는
+    // resize이벤트나, orientationchange이벤트를 이용하여 값이 변경될때마다 아래 함수를 실행 시켜 주시거나,
+    // 직접 element_layer의 top,left값을 수정해 주시면 됩니다.
+    function initLayerPosition(){
+        var width = 300; //우편번호서비스가 들어갈 element의 width
+        var height = 400; //우편번호서비스가 들어갈 element의 height
+        var borderWidth = 5; //샘플에서 사용하는 border의 두께
+
+        // 위에서 선언한 값들을 실제 element에 넣는다.
+        element_layer.style.width = width + 'px';
+        element_layer.style.height = height + 'px';
+        element_layer.style.border = borderWidth + 'px solid';
+        // 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
+        element_layer.style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/2 - borderWidth) + 'px';
+        element_layer.style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 'px';
+    }
     /*===========================================================================*/
     const signUp = () => {
 
@@ -52,8 +141,9 @@ export default function Signup(props) {
         mphone: '',
         msno: '',
         mname: '',
-        madress: '',
     })
+
+
     const [confirm, setConfirm] = useState({ // form의 input의 객체값이 전부 true여야 통과
         midc: false,
         mpwc: false,
@@ -98,7 +188,8 @@ export default function Signup(props) {
     }
     const pwfirmcheck = (e) => {/*비밀번호 확인 [2022-12-16] 김원종 */
         setForm({...form, mpwconfirm: e.target.value})
-
+        console.log(form.mpw)
+        console.log(e.target.value)
         let pwchbox = document.querySelector('.pwchbox')
         if (form.mpw === e.target.value) {
             pwchbox.innerHTML = "👀비밀번호가 일치합니다👀"
@@ -142,8 +233,6 @@ export default function Signup(props) {
             mnameckbox.innerHTML = "나라가 어디신가요..?😳관리자에게 문의해주세요..ㅠㅠ"
         }
     }
-
-
 
 
     const madresscheck = (e) => {
@@ -207,24 +296,35 @@ export default function Signup(props) {
                 <div>
                     <span className="mnameckbox"></span>
                 </div>
-
-                <label className="text-bg-center">👉주소</label> {/*2022-12-19 김원종 주소 api 구현중*/}
-                <div className="address_search">
-                    <input  className="form-control madress"  value={form.madress=address.name} placeholder="주소" type="text"
-                           onChange={(e) => {
-                               madresscheck(e);
-                           }}/>
-                    <button type='button' onClick={handleClick}>
-                        위치찾기
-                    </button>
+                {/*주소 ifram 방식으로 변경 2022-12-21 김원종 */}
+                <label className="text-bg-center">👉주소</label><br/>{/*2022-12-19 김원종 주소 api 구현중*/}
+                <input type="text" className="form-control" id="sample2_postcode" placeholder="우편번호"/>
+                <input type="button" className="form-control" onClick={sample2_execDaumPostcode} value="우편번호 찾기"/><br/>
+                <input type="text" className="form-control" id="sample2_address" placeholder="주소"/><br/>
+                <input type="text" className="form-control" id="sample2_detailAddress" placeholder="상세주소"/>
+                <input type="text" className="form-control" id="sample2_extraAddress" placeholder="참고항목"/>
+                <div ref={element_layer}
+                     style={{
+                         display: "none",
+                         position: "fixed",
+                         overflow: "hidden",
+                         zIndex: "1",
+                         WebkitOverflowScrolling: "touch"
+                     }}>
+                    <img src="//t1.daumcdn.net/postcode/resource/images/close.png=btnCloseLayer"
+                         style={{cursor: "pointer", position: "absolute", right: "-3px", top: "-3px", zIndex: "1"}}
+                         onClick={closeDaumPostcode} alt="닫기 버튼"></img>
                 </div>
-
-
-
 
                 <button type="button" onClick={signUp}>Go</button>
             </form>
         </div>
     );
 }
+
+
+
+
+
+
 
