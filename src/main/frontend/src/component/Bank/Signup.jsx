959ebf3b35231d39bc9cbd65a34077cd870960b2
김원종 <mz1693@naver.com>
2022-12-27 11:03:39 +0900

    2022-12-27 1차 커밋

--- src/main/frontend/src/component/Bank/Signup.jsx
+++ src/main/frontend/src/component/Bank/Signup.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import "../css/Signup.css"
-import DaumPostcode, {useDaumPostcodePopup  } from "react-daum-postcode";
+import DaumPostcode, {useDaumPostcodePopup} from "react-daum-postcode";
 import DaumPostcodeEmbed from "react-daum-postcode";
 
 
@@ -31,17 +31,10 @@ export default function Signup(props) {
     /*===========================================================================*/
     /*카카오 주소 api*/
 
-
-    const element_layer = useRef( null );
-
-    function closeDaumPostcode() {
-        // iframe을 넣은 element를 안보이게 한다.
-        element_layer.style.display = 'none';
-    }
-    function sample2_execDaumPostcode() {
-        new DaumPostcodeEmbed({
+    function sample6_execDaumPostcode() {
+        new daum.Postcode({
             oncomplete: function(data) {
-                // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
                 // 각 주소의 노출 규칙에 따라 주소를 조합한다.
                 // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
@@ -71,49 +64,19 @@ export default function Signup(props) {
                         extraAddr = ' (' + extraAddr + ')';
                     }
                     // 조합된 참고항목을 해당 필드에 넣는다.
-                    document.getElementById("sample2_extraAddress").value = extraAddr;
+                    document.getElementById("sample6_extraAddress").value = extraAddr;
 
                 } else {
-                    document.getElementById("sample2_extraAddress").value = '';
+                    document.getElementById("sample6_extraAddress").value = '';
                 }
 
                 // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                document.getElementById('sample2_postcode').value = data.zonecode;
-                document.getElementById("sample2_address").value = addr;
+                document.getElementById('sample6_postcode').value = data.zonecode;
+                document.getElementById("sample6_address").value = addr;
                 // 커서를 상세주소 필드로 이동한다.
-                document.getElementById("sample2_detailAddress").focus();
-
-                // iframe을 넣은 element를 안보이게 한다.
-                // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
-                element_layer.style.display = 'none';
-            },
-            width : '100%',
-            height : '100%',
-            maxSuggestItems : 5
-        }).embed(element_layer);
-
-
-        // iframe을 넣은 element를 보이게 한다.
-        element_layer.style.display = 'block';
-
-        // iframe을 넣은 element의 위치를 화면의 가운데로 이동시킨다.
-        initLayerPosition();
-    }
-    // 브라우저의 크기 변경에 따라 레이어를 가운데로 이동시키고자 하실때에는
-    // resize이벤트나, orientationchange이벤트를 이용하여 값이 변경될때마다 아래 함수를 실행 시켜 주시거나,
-    // 직접 element_layer의 top,left값을 수정해 주시면 됩니다.
-    function initLayerPosition(){
-        var width = 300; //우편번호서비스가 들어갈 element의 width
-        var height = 400; //우편번호서비스가 들어갈 element의 height
-        var borderWidth = 5; //샘플에서 사용하는 border의 두께
-
-        // 위에서 선언한 값들을 실제 element에 넣는다.
-        element_layer.style.width = width + 'px';
-        element_layer.style.height = height + 'px';
-        element_layer.style.border = borderWidth + 'px solid';
-        // 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
-        element_layer.style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/2 - borderWidth) + 'px';
-        element_layer.style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 'px';
+                document.getElementById("sample6_detailAddress").focus();
+            }
+        }).open();
     }
     /*===========================================================================*/
     const signUp = () => {
@@ -252,13 +215,15 @@ export default function Signup(props) {
     return (
         <div>
             <form className="bankform">
-                <label className="text-bg-center" style={{color:"white"}}>👉아이디[ 영대소문자,특수문자,숫자 포함 6~15자리로 작성해주세요😀]</label>
+                <label className="text-bg-center" style={{color: "white"}}>👉아이디[ 영대소문자,특수문자,숫자 포함 6~15자리로
+                    작성해주세요😀]</label>
                 <input type="text" name="mid" maxLength="15" value={form.mid} className="form-control"
                        onChange={(e) => midcheck(e)}/>
                 <div>
                     <span className="idbox"></span>
                 </div>
-                <label className="text-bg-center" style={{color:"white"}}>👉비밀번호 [최소 8 자 및 최대 10 자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상으로
+                <label className="text-bg-center" style={{color: "white"}}>👉비밀번호 [최소 8 자 및 최대 10 자, 대문자 하나 이상, 소문자 하나,
+                    숫자 하나 및 특수 문자 하나 이상으로
                     작성해주세요😁]</label>
                 <input type="password"
                        maxLength="15" className="form-control" value={form.mpw} name="mpw"
@@ -267,7 +232,7 @@ export default function Signup(props) {
                     <span className="pwbox"></span>
                 </div>
 
-                <label className="text-bg-center" style={{color:"white"}}>👉비밀번호확인</label>
+                <label className="text-bg-center" style={{color: "white"}}>👉비밀번호확인</label>
                 <input type="password"
                        className="form-control" value={form.mpwconfirm} name="mpwconfirm"
                        onChange={(e) => pwfirmcheck(e)}/>
@@ -275,32 +240,33 @@ export default function Signup(props) {
                     <span className="pwchbox"></span>
                 </div>
 
-                <label className="text-bg-center" style={{color:"white"}}>👉전화번호</label>
+                <label className="text-bg-center" style={{color: "white"}}>👉전화번호</label>
                 <input type="text" value={form.mphone}
                        className="form-control " name="mphone" maxLength="13" onChange={(e) => phonecheck(e)}/>
                 <div>
                     <span className="phckbox"></span>
                 </div>
 
-                <label className="text-bg-center" style={{color:"white"}}>👉주민번호[ - 을 제외하고 적어주세요 ]</label>
+                <label className="text-bg-center" style={{color: "white"}}>👉주민번호[ - 을 제외하고 적어주세요 ]</label>
                 <input type="password" value={form.msno}
                        className="form-control " name="msno" maxLength="13" onChange={(e) => msnocheck(e)}/>
                 <div>
                     <span className="msnockbox"></span>
                 </div>
-                <label className="text-bg-center" style={{color:"white"}}>👉이름</label>
+                <label className="text-bg-center" style={{color: "white"}}>👉이름</label>
                 <input type="text" value={form.mname}
                        className="form-control " name="mname" onChange={(e) => mnamecheck(e)}/>
                 <div>
                     <span className="mnameckbox"></span>
                 </div>
                 {/*주소 ifram 방식으로 변경 2022-12-21 김원종 */}
-                <label className="text-bg-center" style={{color:"white"}}>👉주소</label><br/>{/*2022-12-19 김원종 주소 api 구현중*/}
-                <input type="text" className="form-control" id="sample2_postcode" placeholder="우편번호"/>
-                <input type="button" className="form-control" onClick={sample2_execDaumPostcode} value="우편번호 찾기"/><br/>
-                <input type="text" className="form-control" id="sample2_address" placeholder="주소"/><br/>
-                <input type="text" className="form-control" id="sample2_detailAddress" placeholder="상세주소"/>
-                <input type="text" className="form-control" id="sample2_extraAddress" placeholder="참고항목"/>
+                <label className="text-bg-center"
+                       style={{color: "white"}}>👉주소</label><br/>{/*2022-12-19 김원종 주소 api 구현중*/}
+                <input type="text" id="sample6_postcode" placeholder="우편번호"/>
+                <input type="button" onClick={sample6_execDaumPostcode} value="우편번호 찾기"/><br/>
+                <input type="text" id="sample6_address" placeholder="주소"/><br/>
+                <input type="text" id="sample6_detailAddress" placeholder="상세주소"/>
+                <input type="text" id="sample6_extraAddress" placeholder="참고항목"/>
                 <div ref={element_layer}
                      style={{
                          display: "none",
